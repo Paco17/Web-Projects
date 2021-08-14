6a31d838ce7e1f505ac530dfcf44b2abc5cb19d1
Paco17 <A01636425@itesm.mx>
2021-08-14 07:14:59 -0500

    Initial setup maze

---
+++ JS-Maze/maze.js
@@ -0,0 +1,47 @@
+//Initialize canvas
+let maze = document.querySelector(".maze");
+let ctx = maze.getContext("2d");
+
+let current;
+
+class Maze{
+    constructor(size, rows, columns){
+        this.size = size;
+        this.rows = rows;
+        this.columns = columns;
+        this.grid = [];
+        this.stack = [];
+    }
+
+    setup(){
+        for(let r=0; r<this.rows; r++){
+            let row = [];
+            for(let c  = 0; c<this.columns; c++){
+                let cell = new Cell(r,c,this.grid,this.size);
+                row.push(cell);
+            }
+            this.grid.push(row);
+        }
+        current = this.grid[0][0];
+    }
+
+}
+
+class Cell{
+    constructor(rowNum, colNum, parentGird, parentSize){
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+        this.parentGrid = parentGrid;
+        this.parentSize = parentSize;
+        this.visited = false;
+        this.walls = {
+            topWall:true,
+            rightWall:true,
+            bottomWall:true,
+            leftWall:true,
+        }
+    }
+}
+
+let newMaze = new Maze(500,10,10);
+newMaze.setup();
